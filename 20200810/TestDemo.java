ae25204f8c9e7ec1d21a1d7954245433fd67755b
孙 <1695432069@qq.com>
2020-08-10 17:52:05 +0800

    练习

---
+++ 20200810/TestDemo.java
@@ -0,0 +1,34 @@
+public class TestDemo{
+	public static void main(String[] args) {
+		for (int i =0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+   		System.out.println("kaixin");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*public static void main(String[] args) {
+		 long a =10L;
+
+		System.out.println(a);
+		System.out.println("最大值：" +Long.MAX_VALUE);
+		System.out.println("最小值：" +Long.MIN_VALUE); //拼接
+
+	}*/
+}   
+	                    
+	       
